33b7f69128607ec148410260319f4ff0b3ff5164
shnnxn <shnnxn@localhost.localdomain>
2018-03-06 23:15:40 +0530

    initial working project

---
+++ app.py
@@ -0,0 +1,9 @@
+from stream_cipher import user_text
+from keydHash import input_message
+
+def main():
+    input_text = "Farhan the Great"
+    hash_message = "te your mssage here "
+    input_message(hash_message)
+    user_text(input_text)
+main()
