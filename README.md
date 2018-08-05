c38a0d7204eefb080bd5f52e7a324f2c44d387fa
shnnxn <shnnxn@localhost.localdomain>
2018-08-05 15:04:33 +0530

    adding readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# shakx
-stream cipher based on hash function
+# Description
+A stream cipher based on the Sha-1 hash function.The project is divided into two modules
+
+-Encryption Module
+-Sharing of keys over the netowrk
+
+# Encryption Module
+![Semantic description of image](1.png "Image Title")
+ 
--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from stream_cipher import user_text
 from keydHash import input_message
 
 def main():
-    input_text = "Farhan the Great"
-    hash_message = "the your mssage here "
+    input_text = "Your plain text"
+    hash_message = "Your hash message"
     input_message(hash_message)
     user_text(input_text)
 
